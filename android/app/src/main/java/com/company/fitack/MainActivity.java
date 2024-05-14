d31ba12b47e400b72b8a1dc8f0bd31f14dcbee07
Areen <109797692+areenyaran@users.noreply.github.com>
2024-05-14 20:29:00 +0300

    solve firebase issue

--- android/app/src/main/java/com/company/fitack/MainActivity.java
+++ android/app/src/main/java/com/company/fitack/MainActivity.java
@@ -1,4 +1,4 @@
-package com.company.fitrackk;
+package com.company.fit;
 
 import io.flutter.embedding.android.FlutterActivity;
 
--- lib/services/firebase_options.dart
+++ lib/services/firebase_options.dart
@@ -2,11 +2,11 @@ import 'package:firebase_core/firebase_core.dart';
 
 class FirebaseOptionsProvider {
   static FirebaseOptions get firebaseOptions => const FirebaseOptions(
-        apiKey: "AIzaSyBhvtVpLha_mcw2C8jJnWaIp2BdkvMmPmY",
-        databaseURL:
-            "https://console.firebase.google.com/u/0/project/fitrackk-a39bb/firestore/databases/-default-/data/~2F",
-        appId: "fitrackk-a39bb",
-        messagingSenderId: '452751692808',
-        projectId: 'fitrackk-a39bb',
-      );
+      apiKey: "AIzaSyAzxiu3VuHJkddnUkrjfZAxyB2_SK-6giU",
+      authDomain: "fitrack-ar138.firebaseapp.com",
+      projectId: "fitrack-ar138",
+      storageBucket: "fitrack-ar138.appspot.com",
+      messagingSenderId: "222905056983",
+      appId: "1:222905056983:web:d49f2c04540df791ab3a92",
+      measurementId: "G-47T8MZ0J54");
 }
