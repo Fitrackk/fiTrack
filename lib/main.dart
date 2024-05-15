ad8b501ec890120a5c639a1da7779d0ff6f42a0a
Ibrahemherzallah <ibrahemherzallah@gmail.com>
2024-05-15 19:45:06 +0300

    Create Dashboard page with sengelton logic and solve conflicts

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,13 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:fitrack/configures/routes.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:fitrack/services/firebase_service.dart';
-import 'package:fitrack/utils/customs/bottom_nav.dart';
 import 'package:fitrack/view_models/challenges.dart';
 import 'package:fitrack/view_models/user.dart';
-import 'package:fitrack/views/dashboard_page.dart';
-import 'package:fitrack/views/sign_up_page.dart';
-import 'package:fitrack/views/signing_page.dart';
+import 'package:fitrack/views/get_started_page.dart';
+import 'package:fitrack/utils/customs/bottom_nav.dart';
 import 'package:flutter/material.dart';
 import 'package:stator/stator.dart';
 
-import 'models/challenge_model.dart';
+import 'configures/routes.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,25 +16,32 @@ void main() async {
 }
 
 class MainApp extends StatefulWidget {
-  const MainApp({super.key});
+  const MainApp({Key? key}) : super(key: key);
 
   @override
-  State<MainApp> createState() => _MainAppState();
+  _MainAppState createState() => _MainAppState();
 }
 
 class _MainAppState extends State<MainApp> {
+  late User? _user = FirebaseAuth.instance.currentUser;
+
   @override
   void initState() {
     super.initState();
     registerSingleton(UserVM());
     registerSingleton(ChallengesVM());
+    FirebaseAuth.instance.authStateChanges().listen((user) {
+      setState(() {
+        _user = user;
+      });
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Signing(),
+      home: _user == null ? const StartedPage() : const BottomNav(),
       routes: Routes.getRoutes(context),
     );
   }
