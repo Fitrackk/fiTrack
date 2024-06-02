40754e823ace83409a9b98aca67b56d465713705
abdullah talahema <3bood.tala7m@gmail.com>
2024-06-02 21:00:15 +0300

    delete old notifications

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:fitrack/services/firebase_service.dart';
 import 'package:fitrack/utils/customs/bottom_nav.dart';
 import 'package:fitrack/view_models/activity_tracking.dart';
 import 'package:fitrack/view_models/challenges.dart';
+import 'package:fitrack/view_models/notifications.dart';
 import 'package:fitrack/view_models/water_reminder_logic.dart';
 import 'package:fitrack/view_models/user.dart';
 import 'package:fitrack/views/get_started_page.dart';
@@ -27,6 +28,9 @@ void main() async {
 
   WaterReminderVM notificationViewModel = WaterReminderVM();
   await notificationViewModel.initializeWaterReminder();
+
+  final notifications = NotificationsVM();
+  await notifications.deleteOldNotifications();
 
   runApp(const MainApp());
 }
